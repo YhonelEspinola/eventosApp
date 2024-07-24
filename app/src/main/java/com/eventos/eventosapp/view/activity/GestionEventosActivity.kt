9a75831fae33d479a9a85223d24e4d09c652fd19
vhuertas <pvhuertas@cibertec.edu.pe>
2024-07-23 23:42:14 -0500

    perfilFragment

--- app/src/main/java/com/eventos/eventosapp/view/activity/GestionEventosActivity.kt
+++ app/src/main/java/com/eventos/eventosapp/view/activity/GestionEventosActivity.kt
@@ -5,16 +5,23 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.eventos.eventosapp.R
+import com.google.firebase.auth.FirebaseAuth
 
 class GestionEventosActivity: AppCompatActivity()  {
+
+    private var  firebaseAuth: FirebaseAuth?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_gestion_eventos)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         val btnCrearEvento = findViewById<Button>(R.id.btnCrearEvento)
         val btnVerEventos = findViewById<Button>(R.id.btnVerEventos)
+        val btnCerrar = findViewById<AppCompatButton>(R.id.btnButonCerrarSesion)
 
 
         btnCrearEvento.setOnClickListener {
@@ -23,5 +30,15 @@ class GestionEventosActivity: AppCompatActivity()  {
         btnVerEventos.setOnClickListener {
             startActivity(Intent(this, ListaEventoActivity::class.java))
         }
+        btnCerrar.setOnClickListener {
+            cerrarSesion()
+        }
     }
+
+    private fun cerrarSesion(){
+        firebaseAuth!!.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finishAffinity()
+    }
+
 }
--- app/src/main/java/com/eventos/eventosapp/view/fragment/PerfilFragment.kt
+++ app/src/main/java/com/eventos/eventosapp/view/fragment/PerfilFragment.kt
@@ -1,13 +1,19 @@
 package com.eventos.eventosapp.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.eventos.eventosapp.R
+import com.eventos.eventosapp.view.activity.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class PerfilFragment: Fragment() {
+
+    private var  firebaseAuth: FirebaseAuth?=null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +25,21 @@ class PerfilFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val cerarSesion : AppCompatButton = view.findViewById(R.id.cerra_sesion)
+
+        cerarSesion.setOnClickListener {
+            cerrarSesion()
+        }
     }
 
+    private fun cerrarSesion(){
+        firebaseAuth!!.signOut()
+        startActivity(Intent(activity,LoginActivity::class.java))
+        activity?.finish()
+    }
     companion object{
         fun newInstance() : PerfilFragment = PerfilFragment()
     }
