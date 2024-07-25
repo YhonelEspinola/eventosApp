3662f85e77d30abefe854ac8d8335d6e8db913cf
AdrianJyQ <adrian.garcia.alcantara.jyq@gmail.com>
2024-07-25 11:07:33 -0500

    BuscarActivity implementado
    Puedes buscar por nombre del profesor o por nombre del evento

--- app/src/main/java/com/eventos/eventosapp/view/activity/BuscarEventoActivity.kt
+++ app/src/main/java/com/eventos/eventosapp/view/activity/BuscarEventoActivity.kt
@@ -3,12 +3,52 @@ package com.eventos.eventosapp.view.activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.eventos.eventosapp.R
+import com.eventos.eventosapp.view.adapter.ListaEventoAdapter
+import com.eventos.eventosapp.viewmodel.EventoViewModel
 
 class BuscarEventoActivity: AppCompatActivity() {
+
+    private lateinit var viewModel: EventoViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_buscar_evento)
+
+        viewModel = ViewModelProvider(this)[EventoViewModel::class.java]
+
+        val recycler =findViewById<RecyclerView>(R.id.recyclerSearch)
+        val searchView = findViewById<SearchView>(R.id.search)
+
+        val adapterE = ListaEventoAdapter()
+        recycler.adapter=adapterE
+        recycler.layoutManager= LinearLayoutManager(this)
+
+        viewModel.listarEvento()
+        viewModel.listEventos.observe(this) {
+
+            if (it.isNotEmpty()) {
+                adapterE.setDatos(it)
+            }
+
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { adapterE.filtrarDatos(it) }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { adapterE.filtrarDatos(it) }
+                return false
+            }
+        })
+
     }
 }
--- app/src/main/java/com/eventos/eventosapp/view/adapter/ListaEventoAdapter.kt
+++ app/src/main/java/com/eventos/eventosapp/view/adapter/ListaEventoAdapter.kt
@@ -7,7 +7,8 @@ import com.eventos.eventosapp.model.Evento
 import com.eventos.eventosapp.view.viewholder.ListaEventoViewHolder
 
 class ListaEventoAdapter: RecyclerView.Adapter<ListaEventoViewHolder>() {
-    private var list= emptyList<Evento>()
+    private var list = emptyList<Evento>()
+    private var listFilter : List<Evento> = list
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaEventoViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -15,16 +16,29 @@ class ListaEventoAdapter: RecyclerView.Adapter<ListaEventoViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return listFilter.size
     }
 
     override fun onBindViewHolder(holder: ListaEventoViewHolder, position: Int) {
-        val evento=list[position]
+        val evento=listFilter[position]
         holder.bind(evento)
     }
 
     fun setDatos(datos: List<Evento>) {
         list = datos
+        listFilter = datos
+        notifyDataSetChanged()
+    }
+
+    fun filtrarDatos (query: String) {
+        listFilter = if (query.isEmpty()) {
+            list
+        } else {
+            list.filter {
+                it.nomevento.contains(query, ignoreCase = true) ||
+                        it.nomprofe.contains(query, ignoreCase = true)
+            }
+        }
         notifyDataSetChanged()
     }
 }
