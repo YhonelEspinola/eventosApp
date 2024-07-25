8738e435601741d06405e5ac7dfdefdc42f819ce
Ronal-lez <ronaldolezamagarcia@gmail.com>
2024-07-24 19:30:00 -0500

    ....

--- app/src/main/java/com/eventos/eventosapp/view/fragment/GuardarFragment.kt
+++ app/src/main/java/com/eventos/eventosapp/view/fragment/GuardarFragment.kt
@@ -1,11 +1,14 @@
 package com.eventos.eventosapp.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.eventos.eventosapp.R
+import com.eventos.eventosapp.view.activity.BuscarEventoActivity
 
 class GuardarFragment: Fragment() {
 
@@ -19,6 +22,12 @@ class GuardarFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val iconBuscar = view.findViewById<ImageView>(R.id.icon_buscar)
+
+        iconBuscar.setOnClickListener{
+            startActivity(Intent(activity, BuscarEventoActivity::class.java))
+        }
     }
 
     companion object{
--- app/src/main/java/com/eventos/eventosapp/view/fragment/PerfilFragment.kt
+++ app/src/main/java/com/eventos/eventosapp/view/fragment/PerfilFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.eventos.eventosapp.R
+import com.eventos.eventosapp.view.activity.BuscarEventoActivity
 import com.eventos.eventosapp.view.activity.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
 
@@ -28,7 +30,12 @@ class PerfilFragment: Fragment() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        val iconBuscar = view.findViewById<ImageView>(R.id.icon_buscar)
         val cerarSesion : AppCompatButton = view.findViewById(R.id.cerra_sesion)
+
+        iconBuscar.setOnClickListener{
+            startActivity(Intent(activity, BuscarEventoActivity::class.java))
+        }
 
         cerarSesion.setOnClickListener {
             cerrarSesion()
